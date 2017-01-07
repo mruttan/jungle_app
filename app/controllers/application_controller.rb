6c8117d0bd09da4cb9d89f10048895ea7aa83bf1
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-01-07 04:06:02 +0000

    Completed User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  
   private
 
   def cart
