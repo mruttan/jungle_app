fa25f6acc26a17ed2d6a7e1de3198b48c89cbf69
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-01-07 04:28:46 +0000

    Completed admin-security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def show
+    puts ENV['USERNAME']
+    puts ENV['PASSWORD']
   end
+
 end
